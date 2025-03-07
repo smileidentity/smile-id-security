cff38672e549d157dbe9b48a5a67739cc2e60897
robin <48369660+robin-ankele@users.noreply.github.com>
2025-03-07 16:19:20 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "SmileIDSecurity",
             url: "https://github.com/smileidentity/smile-id-security/Releases/0.0.0/SmileIDSecurity.xcframework.zip",
-            checksum: "f05cc236fd9e99258d950dbec1ffb2219b14cdfa56deca2e01608880615b40d7"
+            checksum: "f05cc236fd9e99258d950dbec1ffb2219b14cdfa56deca2e88880615b40d7"
         )
     ]
 )
